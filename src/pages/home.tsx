95ec71f47c34ea0370510c9d16e4afd246080f4b
Asad Ali <asadali.code123@gmail.com>
2022-12-30 12:53:23 +0500

    Ui styles fixed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,11 +4,16 @@ import BackroundImage from "../utils/backgroundImage";
 import TopBg from "../assets/home/1_web.png";
 import Section1Bg from "../assets/home/section1_bg.svg";
 import Section2Bg from "../assets/home/section2_bg.svg";
+import Section2MobBg from "../assets/home/section2_mob_bg.png";
+
+
 import Section3Bg from "../assets/home/section3_bg.svg";
 import Section2UpperBg from "../assets/home/section2_upper_bg.png";
 import Section3UpperBg from "../assets/home/section3_upper_bg.png";
 import Section4UpperBg from "../assets/home/section4_upper_bg.png";
 import Section5UpperBg from "../assets/home/section5_upper_bg.png";
+import Section5UpperMobBg from "../assets/home/section5_upper_mob_bg.png";
+
 import Section4Bg from "../assets/home/2.svg";
 
 import TopMobileBg from "../assets/home/1_mobile.png";
@@ -19,17 +24,13 @@ import { ReactComponent as Visa } from "../assets/icons/visa.svg";
 import { ReactComponent as Business } from "../assets/icons/business.svg";
 import { ReactComponent as Investment } from "../assets/icons/investment.svg";
 
-import { ReactComponent as Linkedln } from "../assets/icons/linkedln.svg";
-import { ReactComponent as Fb } from "../assets/icons/fb.svg";
-import { ReactComponent as Twitter } from "../assets/icons/twitter.svg";
-
 import IsMobile from "../utils/detectDevice";
 
 const Home = () => {
 
     return (
         <>
-            <BackroundImage url={IsMobile() ? TopMobileBg : TopBg} height={'100vh'}>
+            <BackroundImage url={IsMobile() ? TopMobileBg : TopBg} classes='bg-contain'>
                 <div className="bg-center-text text-center space-y-6">
                     <h1 className=" text-white  font-bold ">JAVA Business Partnership International</h1>
                     <p className=" text-white opacity-70">Our Partnerships with Global Companies connects You the Business Owner to Products and Services at huge discounted rates that you would not necessarily have access to. We have done all the work for you, by connecting your Business to the World. Our partners have been in business collectively for over 50 years, Java Business Partners International brings expertise and services to you with Pride.</p>
@@ -38,14 +39,12 @@ const Home = () => {
                 </div>
             </BackroundImage>
 
-            <section className="text-center space-y-9 my-16 bg-right"
-                style={{
-                    backgroundImage: `url(${Section1Bg})`,
-                }} >
-                <h2 className="font-bold">Our Services Offered</h2>
+
+            <BackroundImage url={Section1Bg} classes="text-center space-y-9  bg-right">
+                <h2 className="font-bold pt-16">Our Services Offered</h2>
 
 
-                <div className="grid grid-cols-3 gap-10 px-16">
+                <div className="grid grid-cols-3 sm:grid-cols-1 gap-10 px-16 sm:px-8">
                     <div className="p-6 space-y-4 bg-white  rounded-2xl card-shadow ">
                         <IconCompany className="h-16 w-auto" />
                         <h5 className="mb-2 font-bold tracking-tight text-black text-left max-w-xs">Company Formations (Both Local and Offshore)</h5>
@@ -83,93 +82,56 @@ const Home = () => {
                     </div>
                 </div>
 
-            </section>
-
-            <section className="text-center space-y-9 my-16 bg-left bg-white"
-                style={{
-                    backgroundImage: `url(${Section2Bg})`,
-                }} >
+            </BackroundImage>
 
 
-                <div className="grid grid-cols-2 px-16">
-                    <img className="max-w-xl w-4/5 ml-12 pt-20" src={Section2UpperBg} />
-
-                    <div className="space-y-4 pt-32">
-                        <h2 className="font-bold">Service Two Case Study Facts And Figures Example</h2>
-                        <p className="text-gray-dark opacity-90 text-left tracking-wide">We work directly with multiple Private Investors and Venture Capital Companies, Hedge Fund Managers and Angel Investors on an International level. Many of our investors are not known or accessible by the public and include wealthy families and individuals, private equity firms, hedge funds, venture capital firms, investment companies, and other private and institutional investors. Our innovative structure funding Partnerships make sure you are partnered with the Company or Investor that best suites your Business Portfolio and needs. We are committed to fulfilling your needs with the highest level of professionalism, expertise and service. <br /> Private Wealth Management <br /> Maximize Your Impact <br /> Our private wealth advisors offer unparalleled resources, access and guidance to help you maximize your impact and use your wealth in meaningful ways. Our private wealth advisors offer unparalelled resources, access and guidance to help you maximize your impact and use your wealth in meaningful ways. Your advisor is deeply attuned to your goals and values and curates all we has to offer on your behalf <br /> Your advisor is deeply attuned to your goals and values and curates all we have to offer. <br /> Learn more about our services below and sign up today.</p>
-                    </div>
-
+            <section className="grid grid-cols-2 mt-16  sm:grid-cols-1 bg-white">
+                <BackroundImage url={IsMobile() ? Section2MobBg : Section2Bg} classes={`text-center bg-left`}>
+                    <img className="max-w-xl w-4/5 ml-12 pt-20 " src={Section2UpperBg} />
+                </BackroundImage>
+                <div className="space-y-4 pt-32 sm:pt-8 pr-16 sm:px-8 py-2">
+                    <h2 className="font-bold">Service Two Case Study Facts And Figures Example</h2>
+                    <p className="text-gray-dark opacity-90 text-left tracking-wide">We work directly with multiple Private Investors and Venture Capital Companies, Hedge Fund Managers and Angel Investors on an International level. Many of our investors are not known or accessible by the public and include wealthy families and individuals, private equity firms, hedge funds, venture capital firms, investment companies, and other private and institutional investors. Our innovative structure funding Partnerships make sure you are partnered with the Company or Investor that best suites your Business Portfolio and needs. We are committed to fulfilling your needs with the highest level of professionalism, expertise and service. <br /> Private Wealth Management <br /> Maximize Your Impact <br /> Our private wealth advisors offer unparalleled resources, access and guidance to help you maximize your impact and use your wealth in meaningful ways. Our private wealth advisors offer unparalelled resources, access and guidance to help you maximize your impact and use your wealth in meaningful ways. Your advisor is deeply attuned to your goals and values and curates all we has to offer on your behalf <br /> Your advisor is deeply attuned to your goals and values and curates all we have to offer. <br /> Learn more about our services below and sign up today.</p>
                 </div>
-
             </section>
 
-            <section className="text-center space-y-9 my-16 bg-right"
-                style={{
-                    backgroundImage: `url(${Section3Bg})`,
-                }} >
 
 
-                <div className="grid grid-cols-2 px-16">
+            <section className="grid grid-cols-2 mt-16  sm:grid-cols-1 bg-white">
+                <BackroundImage url={Section3Bg} classes="text-center space-y-9 bg-right">
                     <div className="space-y-4 pt-8">
                         <h2 className="font-bold">Why Work With Us?</h2>
                         <p className="text-gray-dark opacity-90 text-left tracking-wide">Convenience <br /> At work, at home - anywhere. <br /> Our form is available online, anytime:PC</p>
                     </div>
+                </BackroundImage>
 
-                    <img className=" pt-8" src={Section3UpperBg} />
-
-
-
-                </div>
-
+                <img className=" pt-8" src={Section3UpperBg} />
             </section>
 
-            <section className="text-center space-y-9 py-10 bg-cover bg-white"
-                style={{
-                    backgroundImage: `url(${Section4Bg})`,
-                }} >
-                <div className="grid grid-cols-2  px-16 ">
-                    <div className="space-y-4 py-8 ">
-                        <h2 className="font-bold text-left text-white w-4/6">Excellence One Step Ahead Of The Pack</h2>
+            <BackroundImage url={Section4Bg} className="bg-contain">
+
+                <section className="grid grid-cols-2 gap-12  px-16 sm:px-8 py-16 sm:grid-cols-1">
+                    <div className="space-y-4 sm:order-2 ">
+                        <h2 className="font-bold text-left py-16 sm:py-0 text-white w-4/6">Excellence One Step Ahead Of The Pack</h2>
                         <p className="text-white opacity-90 text-left tracking-wider">
                             The 21 century has changed the way people do business and live. <br /> Looking for the ultimate Lifestyle, low tax rates, or a visa <br /> that enables visa free travel to over 150 <br /> countries - We are your partner of choice! <br /> Whether you are doing business in Europe, Asia, Africa, <br /> the Middle East, the Americas or elsewhere, <br /> our partners will set up the best trading or holding <br /> structure for your business in line with <br /> local laws and regulations. Our professional <br /> services include but are not limited to <br /> the following. We provide expertise <br /> and support to help you: Conduct business in  the global market smoothly Maximize opportunities <br /> and achieve long-term sustainability Reduced <br /> taxation Secure intellectual property <br /> Fewer restrictions Access to tax <br /> treaties Enhanced assets Political stability <br /> We provide expertise and support conducting business in global <br /> market smoothly, maximize opportunities and achieve long-term sustainability
                         </p>
                     </div>
-                    <img className="py-5 w-4/5 ml-auto " src={Section4UpperBg} />
-                </div>
+                    <img src={Section4UpperBg} />
+
+                </section>
+
+            </BackroundImage>
 
 
-            </section>
-
-            <section className="space-y-9 py-10 bg-cover bg-white"
-                style={{
-                    backgroundImage: `url(${Section5UpperBg})`,
-                }} >
-                <div className=" space-y-6 px-12">
+            <BackroundImage url={IsMobile() ? Section5UpperMobBg : Section5UpperBg} classes="space-y-9  bg-cover">
+                <div className=" space-y-6 px-16 py-20">
                     <h5 className=" text-white text-left font-bold ">Find Out More About Our Service Offerings And How We Can Help You Access Your 20% Discount Off <br /> Our Services?</h5>
                     <h5 className=" text-white text-left font-bold ">Register Online For One Of Our Service Offerings To Qualify For Your Discount</h5>
                     <button className=" rounded border-2 border-yellow-secondary w-auto p-3 ">
                         <p className="text-yellow-secondary font-semibold">Register Online Here Now</p></button>
                 </div>
-            </section>
-
-            <footer className="bg-yellow-secondary px-10 py-8">
-                <div className="flex justify-between items-end">
-                    <h2 className="text-black font-bold pb-4" >JAVABP</h2>
-                    <div>
-                        <h5 className="text-black font-bold" >Follow Us</h5>
-                        <div className="flex gap-4">
-                            <Linkedln className="w-10 sm:w-6" />
-                            <Fb className="w-10 sm:w-6" />
-                            <Twitter className="w-10 sm:w-6" />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="pt-10  text-center">
-                    <p className="font-bold">@{new Date().getFullYear()} Java Business Partners International | Powered by Beaver Builder</p>
-                </div>
-            </footer>
-
+            </BackroundImage>
 
         </>
     );
--- src/utils/backgroundImage.tsx
+++ src/utils/backgroundImage.tsx
@@ -2,22 +2,26 @@ import { useState } from "react";
 import { ReactComponent as Logo } from "../assets/logo.svg";
 
 
-const BackroundImage = ({ url, height, backgroundSize, backgroundAttachment, backgroundPositionY, children }: any) => {
+const BackroundImage = ({ url, classes, children }: any) => {
     return (
         <div style={{
             backgroundImage: `url(${url})`,
-            backgroundRepeat: 'no-repeat',
-            height: height,
-            'backgroundSize': backgroundSize ? backgroundSize : 'contain',
-            'backgroundAttachment': backgroundAttachment ? backgroundAttachment : 'fixed',
-            'backgroundPositionY': backgroundPositionY ? backgroundPositionY : '11vh',
-            // height: height?height:'100vh',
-
-
-        }}>
+        }} className={classes}>
             {children}
         </div>
     )
 }
+
+// {
+//     backgroundImage: `url(${url})`,
+//     backgroundRepeat: 'no-repeat',
+//     height: height,
+//     'backgroundSize': backgroundSize ? backgroundSize : 'contain',
+//     'backgroundAttachment': backgroundAttachment ? backgroundAttachment : 'fixed',
+//     'backgroundPositionY': backgroundPositionY ? backgroundPositionY : '11vh',
+//     // height: height?height:'100vh',
+
+
+// }
 
 export default BackroundImage
